9d503398020d6713e12ac0a37867c73f0c4b24bb
GreefeN <km.abramov@gmail.com>
2021-04-04 15:18:05 +0300

    task 2 (text split) complete

--- task2/Program.cs
+++ task2/Program.cs
@@ -18,6 +18,71 @@ namespace task2
 
         static void Main(string[] args)
         {
+            string text = "AAA ББ ВВВВ Г ДДДД  ДД ЕЕ ЖЖЖЖ ЗЗЗ";
+
+            Console.WriteLine("Слово из текста с минимальной длиной: ");
+            string resultWord = MinWordOutput(text);
+            Console.WriteLine($"{resultWord}\n");
+
+            Console.WriteLine("Слово или слова максимальной длины: ");
+            string[] resultMaxWords = MaxWordsOutput(text);
+            foreach (string e in resultMaxWords) Console.Write($"{e} ");
+            Console.ReadKey();
         }
+
+        /// <summary>
+        /// принимает текст, и возвращает слово с минимальным количеством букв
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static string MinWordOutput(string s)
+        {
+            char[] separators = { '.', ',', ':', ';', ' ' };    //массив для разделителей текста
+            string[] arr = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);  //разбивка входящего текста на массив слов, с удалением пустых строк
+            string minWord = arr[0];    //базовое значение
+
+            for (int i = 1; i < arr.Length; i++)    //перебор массива слов для поиска минимальной длины
+            {
+                minWord = arr[i].Length < minWord.Length ? arr[i] : minWord;
+            }
+
+            return minWord;
+        }
+
+        /// <summary>
+        /// примает текст и возвращает слово(слова) с максимальным количеством букв
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static string[] MaxWordsOutput(string s)
+        {
+            char[] separators = { '.', ',', ':', ';', ' ' };    //массив для разделителей текста
+            string[] arr = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);  //разбивка входящего текста на массив слов, с удалением пустых строк
+            int maxLenght = 0;
+
+            foreach (string e in arr)   //получаем максимальную длину слова в массиве
+            {
+                maxLenght = e.Length > maxLenght ? e.Length : maxLenght;
+            }
+
+            int count = 0; //счётчик длинных слов
+            foreach (string e in arr) //узнаем количество слов в массиве с максимальной длиной, для создания массива содержащего их
+            {
+                if (e.Length == maxLenght) count++;
+            }
+
+            string[] maxWords = new string[count]; //создаем массива максимально длинных слов для вывода
+            int id = 0; //index в массиве длинных слов
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length == maxLenght) //проверяем слово из входящего массива на максимальную длину
+                {
+                    maxWords[id++] = arr[i]; //присваиваем значения массиву длинных слов
+                }
+            }
+
+            return maxWords;
+        }
+
     }
 }
